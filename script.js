c0052331c8362df6b384607ccb9523a8a9850fd5
Axel Xero <schoen9576@gmail.com>
2021-07-21 23:12:36 -0500

    overcomplicated original code rewritten in time crunch to simplify

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-var timeInput = moment().format("MMM Do YYYY, h:mm a")
+var timeDisplayEl = $("#currentDay")
+var saveBtn = $(".saveBtn")
+var plannerInput = $(".desription")
 
-$('#currentDay').html(timeInput);
+function showTime() {
+    var now = moment().format("MMM Do YYYY, h:mm:ss a")
+    timeDisplayEl.text(now)
+}
+setInterval(showTime, 1000)
+
+var currentTime = Number.parseInt(moment().format("kk:mm"));
+
+function saveInput (event) {
+    event.preventDefault();
+
+    plannerInput = $(this).siblings(".description").val();
+    var time = $(this).siblings(".hour").attr("id");
+
+    localStorage.setItem(time, plannerInput);
+
+    if (plannerInput === "") {
+        alert("Please input an item before submitting.")
+    }
+}
+
+saveBtn.on("click", saveInput);
+
+$(".time-block").each(function(){
+    var hour = parseInt($(this).children(".hour").attr("id"));
+    $(this).children(".description").val(localStorage.getItem(hour)); 
+    
+    
+    if(hour == currentTime){
+      $(this).removeClass("present future");
+      $(this).addClass("past");
+    }else if ( hour > currentTime){
+      $(this).removeClass("past future");
+      $(this).addClass("present");
+    }else{
+      $(this).removeClass("past present");
+      $(this).addClass("future");
+    }
+  })
